b96e2322a334e849c88a90809db78580bb784910
kotov001 <rodwellmakoto@msn.com>
2020-08-08 13:10:49 +0200

    Update PrimeCheck.java

--- PrimeCheck.java
+++ PrimeCheck.java
@@ -1,19 +1,19 @@
 public class PrimeCheck {
    public static boolean isPrime(int n,int i){
        if(n<=2)
-         return true;
+         return true;           //base case
          if(n%i==0)
-           return false;;
+           return false;
          if(i*i < n)
          return true;
-         return isPrime(n,i+1);
+         return isPrime(n,i+1);   //recursion
       }
   public static void main(String [] args){
       int num = 17;
        if(isPrime(num,0x2))
          System.out.println("yes its a prime number");
          else
-            System.out.println("nota prime number");
+            System.out.println("not a prime number");
 
 
 }
